e64950029540387140401b9e8c0821b90e842171
Ricardo Dib <ricardo.dib@tectrolnet.local>
2022-06-29 10:25:21 -0300

    commit

---
+++ Start de um determinado jogo.py
@@ -0,0 +1,9 @@
+personagem_nome = 'Ricardo'
+personagem_idade = '25'
+personagem_esporte = 'futebol'
+personagem_jogo = 'GTA'
+
+print('Meu nome é ' + personagem_nome + ',')
+print('eu tenho ' + personagem_idade + ' anos de idade' '.')
+print('Meu esporte favorito é o ' + personagem_esporte + ',' )
+print('eu gosto de jogar ' + personagem_jogo + ' nas minhas horas vagas.')
